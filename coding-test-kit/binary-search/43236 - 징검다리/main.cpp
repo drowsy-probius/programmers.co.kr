4a1a12ac39e76eee0c2118ac2d8042d06c9912e1
k123s456h <k123s456h@gmail.com>
2021-06-24 10:53:57 +0900

    init 43236

---
+++ coding-test-kit/binary-search/43236 - 징검다리/main.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int distance, vector<int> rocks, int n) {
+  int answer = 0;
+  return answer;
+}
+
+#include "iostream"
+#include "cassert"
+int main(void)
+{
+  assert(solution(25, {2, 14, 11, 21, 17}, 2) == 4);
+  return 0;
+}
