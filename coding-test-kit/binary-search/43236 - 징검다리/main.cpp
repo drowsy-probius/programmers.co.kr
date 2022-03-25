3fd0da60cee5c85cc078c46998cd6734a0ff7920
k123s456h <k123s456h@gmail.com>
2022-03-25 15:30:58 +0900

    >_<

--- coding-test-kit/binary-search/43236 - 징검다리/main.cpp
+++ coding-test-kit/binary-search/43236 - 징검다리/main.cpp
@@ -2,45 +2,47 @@
 #include <vector>
 #include <algorithm>
 
-#include "iostream"
-
 using namespace std;
 
 int solution(int distance, vector<int> rocks, int n) {
-  int answer = 0;
-  vector<int> space;
-
-  sort(rocks.begin(), rocks.end(), less<int>());
-
+  /**
+   * i-th idx: distance from i-th rock to (i+1)-th rock
+   */
+  vector<int> spaces;
+  
+  sort(rocks.begin(), rocks.end());
+  rocks.push_back(distance);
   for(int i=0; i<rocks.size(); i++)
   {
-    if(i==0) space.push_back(rocks.at(i));
-    else space.push_back(rocks.at(i) - rocks.at(i-1));
+    int before = i>0 ? rocks.at(i-1) : 0;
+    spaces.push_back(rocks.at(i) - before);
   }
 
   while(n--)
   {
-    vector<int>::iterator minimal = min_element(space.begin(), space.end());
-    int min_idx = std::distance(space.begin(), minimal);
+    int min_idx = min_element(spaces.begin(), spaces.end()-1) - spaces.begin();
+    int min_value = spaces.at(min_idx);
     
-    if(min_idx + 1 == space.size())
+    if(min_idx > 0 && spaces.at(min_idx - 1) < spaces.at(min_idx + 1))
     {
-      space.at(min_idx - 1) += space.at(min_idx);
+      spaces.at(min_idx - 1) += min_value;
     }
     else
     {
-      space.at(min_idx + 1) += space.at(min_idx);
+      spaces.at(min_idx + 1) += min_value;
     }
-    space.erase(minimal);
+    spaces.erase(spaces.begin() + min_idx);
   }
 
-  return *min_element(space.begin(), space.end());
+
+  return *min_element(spaces.begin(), spaces.end());
 }
 
 #include "cassert"
 int main(void)
 {
   // assert(solution(25, {2, 14, 11, 21, 17}, 2) == 4);
-  assert(solution(20, {15, 16, 17, 18, 19}, 2) == 2);
+  // assert(solution(20, {15, 16, 17, 18, 19}, 2) == 1);
+  assert(solution(10, {3, 5, 7}, 2) == 5);
   return 0;
 }
